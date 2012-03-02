0c04d50de7bbf17efd1deb0381d095a4186f4687
Jun Tachikawa <jun.tachikawa@gmail.com>
2012-03-02 18:54:53 +0900

    modify test.v

--- test.v
+++ test.v
@@ -1,4 +1,37 @@
-module hoge(a,b,foo,bar)
+module add(
+    input [3:0] a,
+    input [3:0] b,
+    output [3:0] c);
 
+   reg [3:0]     c;
+
+   always@* begin
+      c <= a + b;
+   end
+   
 endmodule
 
+
+module main;
+   reg [3:0] a, b;
+   wire [3:0] c;
+   
+   add add0(.a(a), .b(b), .c(c));
+
+   task set(input [3:0] in1, input [3:0] in2);
+      begin
+         a = in1;
+         b = in2;
+         #1 $display("a=%d, b=%d, c=%d",a,b,c);
+      end
+   endtask
+
+   integer i,j;
+   
+   initial begin
+      for(i=0; i<10; i=i+1)
+        for(j=0; j<10; j=j+1)
+          #10 set(i,j);
+      $finish;
+   end
+endmodule
