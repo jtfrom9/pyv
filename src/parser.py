420ae75b13c72cba80df9df28df272b9245065d5
Jun Tachikawa <jun.tachikawa@gmail.com>
2012-06-06 01:39:34 +0900

    modify

--- src/parser.py
+++ src/parser.py
@@ -6,35 +6,57 @@ module_declaration = Forward()
 list_of_port_declarations = Forward()
 non_port_module_item = Forward()
 
-ID = Word(alphas, alphanums+'_')
-LP = Suppress("(")
-RP = Suppress(")")
-SEMCOLON = Suppress(";")
+RESERVED_KEYWORDS = ('module', 'endmodule')
 
-source_text << ZeroOrMore(description)
-description << module_declaration
 
-#module_declaration << Keyword("module") + ID + list_of_port_declarations + SEMCOLON + ZeroOrMore(non_port_module_item) + Keyword("endmodule")
-module_declaration << Keyword("module") + ID("name") + Group(list_of_port_declarations) + SEMCOLON \
-    + non_port_module_item \
-    + Keyword("endmodule")
+ID = ~MatchFirst([Keyword(w) for w in RESERVED_KEYWORDS]) + Word(alphas, alphanums+'_')
 
-non_port_module_item << ID("body")
+#source_text << ZeroOrMore( Group(description) )("descriptions")
+source_text << ZeroOrMore(description)("descriptions")
+description << Group( Group(module_declaration)("module") ^ ID("id") )("desc")
 
-list_of_port_declarations << LP + ZeroOrMore(ID) + ZeroOrMore( Suppress(",") + ID ) + RP
+module_declaration << Suppress(Keyword("module")) + ID("name") + Group(list_of_port_declarations)("params") + Suppress(";") \
+    + Group(ZeroOrMore(non_port_module_item))("body") \
+    + Suppress(Keyword("endmodule"))
+
+non_port_module_item << ID
+
+list_of_port_declarations << Suppress("(") + ZeroOrMore(ID) + ZeroOrMore( Suppress(",") + ID ) + Suppress(")")
+
+
 
 def main():
-    print list_of_port_declarations.parseString(" ( a,  b,c  , d)")
-    print module_declaration.parseString('''
+#     print list_of_port_declarations.parseString(" ( a,  b,c  , d)")
+    result = source_text.parseString('''
 module hoge(a,b);
+task
+task
+task
+initial begin
+endmodule
+
+A
+B
+
+module hoge2(a1,bb,c,out);
+func
 always
 endmodule
 ''')
-#     print module_declaration.parseString('''
-# module hoge( a, b, c);
-# endmodule
-# ''')
+#     print result
+#     for index,desc in enumerate(result.descriptions):
+#         print "desc[",index,"]=",desc
 
+    print result.dump()
+#     for index,desc in enumerate(result.descriptions):
+#         print desc.dump()
 
+    print result.asXML()
+
+    for index,desc in enumerate(result.descriptions):
+        print index, desc
+        print desc.module, desc.id
+
+    
 if __name__=='__main__':
     main()
