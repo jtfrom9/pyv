fe04e65a518f55d5acf0a8d6ffa923cb95b99eed
Jun Tachikawa <jun.tachikawa@gmail.com>
2012-08-24 01:52:29 +0900

    add testgen.py

---
+++ src/testgen.py
@@ -0,0 +1,9 @@
+
+with open("non_terminal_symbols.txt","r") as f:
+    for index,sym in enumerate(f):
+        print('''
+@TestCase2(grammar.{sym})
+def test{index}(self):
+	pass
+'''.format(sym=sym.strip(), index=index))
+
